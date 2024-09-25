f6606fc8a1b9710fe83cdc17b521e7e1a5fab723
Yang Jing <yang.xunjing@qq.com>
2024-09-25 17:40:26 +0800

    chore: 添加 protobuf 描述符文件生成配置

--- tonic-getting/build.rs
+++ tonic-getting/build.rs
@@ -1,7 +1,17 @@
+use std::{env, path::PathBuf};
+
 fn main() {
+  let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
   tonic_build::configure()
     // 显示启用当 .proto 文件变化时自动重编译
     .emit_rerun_if_changed(true)
+    .build_server(true)
+    .build_client(true)
+    // 生成描述符文件，当使用 gRPC Reflection 功能时可以从这个文件中获取服务描述信息来返回给调用方
+    .file_descriptor_set_path(out_dir.join("getting_descriptor.bin"))
+    // 设置代码生成器输出目录，默认为 OUT_DIR 环境变量指定目录
+    .out_dir(out_dir)
     .compile(
       &[
         "proto/getting/basic.proto",
